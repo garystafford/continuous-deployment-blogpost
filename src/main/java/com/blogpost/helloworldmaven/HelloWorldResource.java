b9392d3026f2fd75fccc9d6df9ee728ead4e5bde
gstaffor <gstaffor@DEV-M-45DFL.paychex.com>
2013-10-27 23:04:16 -0400

    Minor clean-up of code

--- src/main/java/com/blogpost/helloworldmaven/HelloWorldResource.java
+++ src/main/java/com/blogpost/helloworldmaven/HelloWorldResource.java
@@ -72,7 +72,7 @@ public class HelloWorldResource {
     @GET
     @Produces("text/html")
     public String getXml() {
-        return String.format("<html><body><h1>Hello {0}!</h1></body></html>",
+        return String.format("<html><body><h1>Hello %s!</h1></body></html>",
                 nameStorage.getName());
     }
 
